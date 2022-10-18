21a04d71097c110d61f9598ef8354dd4d91042c0
David Conde <dctoo42me@gmail.com>
2022-10-18 04:20:06 -0500

    create display numbers and clear display button function

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+
+let display = document.querySelector('.display h2');
+let addition = document.querySelector('.add');
+let numbers = [];
+display.textContent = 0;
+
 function add(arr) {
     const result = arr.reduce((accumulator ,currVal) => { 
         return accumulator + currVal; 
@@ -29,3 +35,26 @@ function divide(arr) {
 function operate(num1, num2, operator) {
     return operator([num1, num2]);
 }
+
+function setUp() {
+    num1 = numbers.join('');
+    console.log('numbers: ',num1);
+}
+setUp();
+
+function displayNumbers() {
+    let btnValue = document.querySelectorAll('div.numbers-container button');
+    btnValue.forEach(btn => btn.addEventListener('click', () => {
+        if(btn.textContent !== 'C' && btn.textContent !== 'blank') {
+            numbers.push(Number(btn.textContent));
+            console.log(btn.textContent);
+            display.textContent = numbers.join('');
+        } else if(btn.textContent === 'C') {
+            numbers.splice(0,numbers.length);
+            display.textContent = 0;
+        }
+    }));
+}
+displayNumbers();
+
+addition.addEventListener('click', setUp);
