b0f80a94aefc3ab898ca233ab5dacec690ae266e
David Conde <dctoo42me@gmail.com>
2022-10-18 20:17:30 -0500

    add event listeners and functionality to operation buttons and submit resultls to display

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-
 let display = document.querySelector('.display h2');
+//operation variables
 let addition = document.querySelector('.add');
+let division = document.querySelector('.divide');
+let multiplication = document.querySelector('.multiply');
+let subtraction = document.querySelector('.subtract');
+let equals = document.querySelector('.equals');
+
 let numbers = [];
-display.textContent = 0;
+let num1;
+let num2;
+let operation;
+display.textContent;
 
 function add(arr) {
     const result = arr.reduce((accumulator ,currVal) => { 
         return accumulator + currVal; 
-    },0);
+    });
     return result;
 }
 
@@ -33,22 +41,25 @@ function divide(arr) {
 }
 
 function operate(num1, num2, operator) {
-    return operator([num1, num2]);
+    let result = operator([num1, num2]);
+    console.log('result: ',result);
+    display.textContent = result;
+    numbers.splice(0,numbers.length);
+    numbers.push(result);
 }
 
 function setUp() {
-    num1 = numbers.join('');
-    console.log('numbers: ',num1);
+    num1 = Number(numbers.join(''));
+    console.log('num1: ',num1);
+    numbers.splice(0,numbers.length);
 }
-setUp();
 
 function displayNumbers() {
     let btnValue = document.querySelectorAll('div.numbers-container button');
     btnValue.forEach(btn => btn.addEventListener('click', () => {
         if(btn.textContent !== 'C' && btn.textContent !== 'blank') {
-            numbers.push(Number(btn.textContent));
-            console.log(btn.textContent);
-            display.textContent = numbers.join('');
+            numbers.push(btn.textContent);
+            display.textContent = Number(numbers.join(''));
         } else if(btn.textContent === 'C') {
             numbers.splice(0,numbers.length);
             display.textContent = 0;
@@ -57,4 +68,28 @@ function displayNumbers() {
 }
 displayNumbers();
 
-addition.addEventListener('click', setUp);
+addition.addEventListener('click', () => {
+    display.textContent = '';
+    operation = add;
+    setUp();
+})
+division.addEventListener('click', () => {
+    display.textContent = '';
+    operation = divide;
+    setUp();
+})
+multiplication.addEventListener('click', () => {
+    display.textContent = '';
+    operation = multiply;
+    setUp();
+})
+subtraction.addEventListener('click', () => {
+    display.textContent = '';
+    setUp();
+})
+
+equals.addEventListener('click', () => {
+    num2 = Number(numbers.join(''));
+    console.log('num2: ',num2);
+    operate(num1,num2,operation);
+})
