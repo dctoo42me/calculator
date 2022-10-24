7ab9292b5c0c51d6e7d13bd13be2b1489d04eb92
David Conde <dctoo42me@gmail.com>
2022-10-23 19:54:23 -0500

    add functionality that allows string of operations to be performed

--- script.js
+++ script.js
@@ -10,8 +10,12 @@ let numbers = [];
 let num1;
 let num2;
 let operation;
-display.textContent;
+let nextOperation;
+let skipOp = false;
+let result = '';
+display.textContent = 0;
 
+//operation functions
 function add(arr) {
     const result = arr.reduce((accumulator ,currVal) => { 
         return accumulator + currVal; 
@@ -40,56 +44,222 @@ function divide(arr) {
     return result;
 }
 
+//produces result on the selected operation 
 function operate(num1, num2, operator) {
-    let result = operator([num1, num2]);
-    console.log('result: ',result);
+    result = operator([num1, num2]);
+    console.log('END OP num1:', num1, 'num2:', num2, operator, 'result:',result);
     display.textContent = result;
-    numbers.splice(0,numbers.length);
-    numbers.push(result);
 }
 
-function setUp() {
-    num1 = Number(numbers.join(''));
-    console.log('num1: ',num1);
+//clear the numbers array
+function numClear() {
     numbers.splice(0,numbers.length);
 }
 
+//iterate over buttons and show selected numbers on display screen in ui
 function displayNumbers() {
     let btnValue = document.querySelectorAll('div.numbers-container button');
     btnValue.forEach(btn => btn.addEventListener('click', () => {
-        if(btn.textContent !== 'C' && btn.textContent !== 'blank') {
+        if(
+            btn.textContent !== 'C' && 
+            btn.textContent !== 'blank') {
             numbers.push(btn.textContent);
             display.textContent = Number(numbers.join(''));
-        } else if(btn.textContent === 'C') {
+            console.log('numbers pushed to array');
+            // console.log('num1 type: ', typeof num1);
+        } else if(
+            btn.textContent === 'C') {
             numbers.splice(0,numbers.length);
             display.textContent = 0;
+            num1 = '';
+            num2 = '';
+            result = '';
+            console.log('clear clicked, nums arr cleared, display cleared, num1 & num2 cleared');
         }
     }));
 }
 displayNumbers();
 
-addition.addEventListener('click', () => {
-    display.textContent = '';
-    operation = add;
-    setUp();
-})
-division.addEventListener('click', () => {
-    display.textContent = '';
-    operation = divide;
-    setUp();
-})
-multiplication.addEventListener('click', () => {
-    display.textContent = '';
-    operation = multiply;
-    setUp();
-})
-subtraction.addEventListener('click', () => {
-    display.textContent = '';
-    setUp();
-})
+//sets the operation based off of selected operator button class
+function funcCheck(e) {
+    let func;
+    console.log('target', e.target);
+    if(
+        e.target.className === 'add') {
+        console.log('PLUS');
+        func = add;
+    } else if(
+        e.target.className === 'subtract') {
+        console.log('MINUS');
+        func = subtract;
+    } else if(
+        e.target.className === 'divide') {
+        console.log('DIVIDE');
+        func = divide;
+    } else if(
+        e.target.className === 'multiply') {
+        console.log('MULTIPLY');
+        func = multiply;
+    }
+    operation = func;
+}
 
-equals.addEventListener('click', () => {
-    num2 = Number(numbers.join(''));
-    console.log('num2: ',num2);
-    operate(num1,num2,operation);
-})
+function nextFuncCheck(e) {
+    let func;
+    // console.log('target', e.target);
+    if(
+        e.target.className === 'equals') {
+            return;
+    } else if (
+        e.target.className === 'add') {
+        console.log('PLUS');
+        func = add;
+    } else if(
+        e.target.className === 'subtract') {
+        console.log('MINUS');
+        func = subtract;
+    } else if(
+        e.target.className === 'divide') {
+        console.log('DIVIDE');
+        func = divide;
+    } else if(
+        e.target.className === 'multiply') {
+        console.log('MULTIPLY');
+        func = multiply;
+    }
+    nextOperation = func;
+} 
+
+//setup nums for operations
+function operateOnNum1AndNum2() {
+    console.log('2nd num1 and num2');
+    if(
+        operation !== undefined &&
+        skipOp === false) {
+            num2 = Number(numbers.join(''));
+            console.log('*****initialized num2',num2);
+            operate(num1,num2,operation);
+            console.log('1 operation performed');
+            num1 = result;
+            result = '';
+            num2 = '';
+            operation = undefined;
+            numClear();
+            console.log('num arr cleared');
+            console.log('num1:', num1, 'num2:', num2, 'result:', result);
+        } else if(
+            operation !== undefined &&
+            nextOperation !== undefined ||
+            skipOp === true) {
+                num2 = Number(numbers.join(''));
+                console.log('*****initialized num2',num2);
+                operate(num1,num2,nextOperation);
+                console.log('2 operation performed');
+                num1 = result;
+                result = '';
+                num2 = '';
+                nextOperation = undefined;
+                skipOp = false;
+                numClear();
+                console.log('num arr cleared');
+                console.log('num1:', num1, 'num2:', num2, 'result:', result);
+        } else if(
+            operation === undefined) {
+                operation = nextOperation;
+                nextOperation = undefined;
+                num2 = Number(numbers.join(''));
+                console.log('*****initialized num2',num2);
+                operate(num1,num2,operation);
+                console.log('next 3 operation performed');
+                num1 = result;
+                result = '';
+                num2 = '';
+                operation = undefined;
+                numClear();
+                console.log('num arr cleared');
+                console.log('num1:', num1, 'num2:', num2, 'result:', result);
+    } else { 
+        console.log('SOMETHING WENT WRONG!');
+    }
+}
+
+//initial path taken to setup initial operation
+function initialSetup(func) {
+    console.log('3rd initial');
+    num1 = Number(numbers.join(''));
+    console.log('*****initialized num1', num1);
+    display.textContent = '';
+    console.log('display cleared');
+    numClear();
+    console.log('num arr cleared');
+}
+
+//start operation with a function check and if statement
+function operationStart(e) {
+    // console.log(e.target.className, 'clicked');
+    console.log('num1:', num1, 'num2:',num2, typeof num2, 'num arr:', numbers, 'result:', result);
+    console.log('operation start');
+    if(
+        operation === undefined &&
+        nextOperation === undefined &&
+        num1 !== undefined && num1 !== '') {
+            funcCheck(e);
+            return;
+    } else if(
+        operation !== undefined &&
+        nextOperation === undefined) { 
+            console.log('checking for nextOp');
+            nextFuncCheck(e);
+            operateOnNum1AndNum2();
+    } else if(
+        operation === undefined &&
+        nextOperation !== undefined ) { 
+            console.log('running nextOp');
+            skipOp = true;
+            funcCheck(e);
+            operateOnNum1AndNum2();
+    } else if(
+        num1 !== '' && num1 !== undefined &&
+        numbers.length > 0) {
+            console.log('num1 has value and num arr > 0');
+            funcCheck(e);
+            operateOnNum1AndNum2();
+    } else { 
+        funcCheck(e);
+        // operation = func;
+        console.log('RAN FUNC CHECK on ELSE');
+    if(
+        num1 !== undefined && num1 !== '') {
+            //2nd
+            operateOnNum1AndNum2();
+        } else {
+            //3rd
+            initialSetup();
+        }
+    }
+}
+
+addition.addEventListener('click', operationStart);
+division.addEventListener('click', operationStart);
+multiplication.addEventListener('click', operationStart);
+subtraction.addEventListener('click', operationStart);
+equals.addEventListener('click', (e) => {
+    if(
+        num1 !== '' || num1 !== undefined &&
+        numbers.length > 1) {
+        console.log('first equals ran')
+        num2 = Number(numbers.join(''));
+        console.log('num1:', num1, 'num2:',num2, typeof num2, 'num arr:', numbers, 'result:', result);
+        operationStart(e);
+    }else { 
+        console.log('second equals ran');
+        num2 = Number(numbers.join(''));
+        console.log('num2: ',num2);
+        operate(num1,num2,operation);
+        num1 = result;
+        result = '';
+        num2 = '';
+        numClear();
+        console.log('num1:', num1, 'num2:', num2, 'result:', result);
+    }
+});
